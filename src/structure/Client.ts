b3bd0d26cb362fd0e705b3443c3769ddfb198d14
zfl4wless <noah.bauditz@live.de>
2022-12-25 12:49:34 +0100

    feat: creates extended client

---
+++ src/structure/Client.ts
@@ -0,0 +1,55 @@
+import { ApplicationCommand, ApplicationCommandDataResolvable, Client, Collection } from 'discord.js';
+import { CommandType } from '../types/Command';
+import glob from 'glob';
+import { promisify } from 'util';
+import { RegisterCommandsOptions } from '../types/Client';
+
+const globPromise = promisify(glob);
+
+export class ExtendedClient extends Client {
+    commands: Collection<string, CommandType> = new Collection();
+
+    constructor() {
+        super({ intents: 32767 });
+    }
+
+    start() {
+        this.registerModules();
+        this.login(process.env.botToken);
+    }
+
+    async importFile(filePath: string) {
+        return (await import(filePath)).default;
+    }
+
+    async registerCommands({ commands, guildId }: RegisterCommandsOptions) {
+        if (guildId) {
+            this.guilds.cache.get(guildId)?.commands.set(commands);
+            console.log(`Registering commands to ${guildId}`);
+        } else {
+            this.application?.commands.set(commands);
+            console.log(`Registering commands to global`);
+        }
+    }
+
+    async registerModules() {
+        // Commands
+        const slashCommands: ApplicationCommandDataResolvable[] = [];
+        const commandFiles = await globPromise(`${__dirname}/../commands/**/*{.ts,.js}`);
+        console.log({ commandFiles });
+        commandFiles.map(async (filePath) => {
+            const command: CommandType = await this.importFile(filePath);
+            if (!command.name) return;
+
+            this.commands.set(command.name, command);
+            slashCommands.push(command);
+        });
+
+        // Events
+        const eventFiles = await globPromise(`${__dirname}/../events/*{.ts,.js}`);
+        eventFiles.map(async (filePath) => {
+            const event = await this.importFile(filePath);
+            this.on(event.name, event.run);
+        });
+    }
+}
---
+++ src/structure/Event.ts
@@ -0,0 +1,7 @@
+import { ClientEvents } from 'discord.js';
+
+export class Event<Key extends keyof ClientEvents> {
+    constructor(public name: Key, public run: (...args: ClientEvents[Key]) => any) {
+        
+    }
+}
